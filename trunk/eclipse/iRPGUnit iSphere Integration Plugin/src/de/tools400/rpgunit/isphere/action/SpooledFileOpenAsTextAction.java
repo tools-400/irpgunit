06c9945b803d3b702a744057bf4d980fc51a6d7b
tools400 <thomas.raddatz@tools400.de>
2018-03-13 14:54:16 +0000

    Initial import.

---
+++ trunk/eclipse/iRPGUnit iSphere Integration Plugin/src/de/tools400/rpgunit/isphere/action/SpooledFileOpenAsTextAction.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2013-2016 iRPGUnit Project Team
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package de.tools400.rpgunit.isphere.action;
+
+import biz.isphere.core.preferencepages.IPreferences;
+import biz.isphere.core.spooledfiles.SpooledFile;
+
+public class SpooledFileOpenAsTextAction extends AbstractSpooledFileAction {
+
+    public static final String ID = "de.tools400.rpgunit.isphere.action.SpooledFileOpenAsTextAction"; //$NON-NLS-1$
+
+    @Override
+    public String execute(SpooledFile spooledFile) {
+        return spooledFile.open(IPreferences.OUTPUT_FORMAT_TEXT);
+    }
+}
