ebb395f7719513524638b383a935567365b8a0d9
tools400 <thomas.raddatz@tools400.de>
2018-03-13 14:54:16 +0000

    Initial import.

---
+++ eclipse/iRPGUnit iSphere Integration Plugin/src/de/tools400/rpgunit/isphere/action/AbstractSpooledFileAction.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2013-2016 iRPGUnit Project Team
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package de.tools400.rpgunit.isphere.action;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+
+import biz.isphere.core.Messages;
+import biz.isphere.core.spooledfiles.SpooledFile;
+
+import com.ibm.etools.iseries.rse.ui.actions.popupmenu.ISeriesAbstractQSYSPopupMenuAction;
+
+import de.tools400.rpgunit.core.extensions.testcase.IRPGUnitTestCaseItem;
+import de.tools400.rpgunit.isphere.factory.ISphereFactory;
+
+public abstract class AbstractSpooledFileAction extends ISeriesAbstractQSYSPopupMenuAction {
+
+    @Override
+    public void run() {
+
+        String message = null;
+
+        Object[] selection = getSelectedRemoteObjects();
+        for (int i = 0; i < selection.length; i++) {
+            if (selection[i] instanceof IRPGUnitTestCaseItem) {
+                IRPGUnitTestCaseItem testCaseItem = (IRPGUnitTestCaseItem)selection[i];
+                SpooledFile spooledFile = ISphereFactory.createSpooledFile(testCaseItem.getSpooledFile());
+                message = execute(spooledFile);
+            }
+            if (message != null) {
+                MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.Error, message);
+                break;
+            }
+        }
+
+    }
+
+    public abstract String execute(SpooledFile spooledFile);
+
+}
