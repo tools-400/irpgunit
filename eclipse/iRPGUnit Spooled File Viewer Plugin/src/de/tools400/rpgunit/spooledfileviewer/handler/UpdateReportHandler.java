0822bec77d81a0572009568333c317e366c0b429
tools400 <thomas.raddatz@tools400.de>
2019-11-17 14:16:12 +0000

    Changed handler to catch error message "CPF3344 File RPGUNIT number 11 no longer in the system.".

--- eclipse/iRPGUnit Spooled File Viewer Plugin/src/de/tools400/rpgunit/spooledfileviewer/handler/UpdateReportHandler.java
+++ eclipse/iRPGUnit Spooled File Viewer Plugin/src/de/tools400/rpgunit/spooledfileviewer/handler/UpdateReportHandler.java
@@ -12,6 +12,7 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 
 import com.ibm.as400.access.AS400;
+import com.ibm.as400.access.AS400Exception;
 import com.ibm.as400.access.SpooledFile;
 import com.ibm.etools.iseries.subsystems.qsys.api.IBMiConnection;
 
@@ -32,15 +33,25 @@ public class UpdateReportHandler implements IRPGUnitUpdateTestResult {
                     AS400 tSystem = IBMiConnection.getConnection(tReportFile.getConnectionName()).getAS400ToolboxObject();
                     SpooledFile tSpooledFile = new SpooledFile(tSystem, tReportFile.getName(), tReportFile.getNumber(), tReportFile.getJobName(),
                         tReportFile.getJobUser(), tReportFile.getJobNumber());
-                    SpooledFileEditorInput tEditorInput = new SpooledFileEditorInput(tReportFile.getTestSuite(), tSpooledFile);
-                    IWorkbenchPage tPage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-                    SpooledFileEditor tEditor = tEditorInput.findEditor(tPage);
-                    if (tEditor != null) {
-                        tEditor.setInput(tEditorInput);
-                        tEditor.updatePartControl(tEditorInput);
+                    // Ensure spooled file exists
+                    tSpooledFile.update();
+                    if (tSpooledFile.getCreateDate() != null) {
+                        SpooledFileEditorInput tEditorInput = new SpooledFileEditorInput(tReportFile.getTestSuite(), tSpooledFile);
+                        IWorkbenchPage tPage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+                        SpooledFileEditor tEditor = tEditorInput.findEditor(tPage);
+                        if (tEditor != null) {
+                            tEditor.setInput(tEditorInput);
+                            tEditor.updatePartControl(tEditorInput);
+                        }
                     }
 
                 } catch (Exception e) {
+                    if (e instanceof AS400Exception) {
+                        AS400Exception as400Exception = (AS400Exception)e;
+                        if (e.getMessage().startsWith("CPF3344")) {
+                            return;
+                        }
+                    }
                     RPGUnitSpooledFileViewer.logError("Could not open spooled file: " + tReportFile.getName(), e); //$NON-NLS-1$
                     return;
                 }
