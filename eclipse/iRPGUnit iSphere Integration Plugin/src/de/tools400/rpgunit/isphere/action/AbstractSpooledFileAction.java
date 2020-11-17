a1d7808172caa34d3f694c94005f5208030fb981
tools400 <thomas.raddatz@tools400.de>
2020-11-17 15:06:50 +0000

    Updated iSphere spooled file actions with CanceledByUserException.

--- eclipse/iRPGUnit iSphere Integration Plugin/src/de/tools400/rpgunit/isphere/action/AbstractSpooledFileAction.java
+++ eclipse/iRPGUnit iSphere Integration Plugin/src/de/tools400/rpgunit/isphere/action/AbstractSpooledFileAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013-2016 iRPGUnit Project Team
+ * Copyright (c) 2013-2020 iRPGUnit Project Team
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,9 +11,11 @@ package de.tools400.rpgunit.isphere.action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 
+import biz.isphere.core.internal.exception.CanceledByUserException;
+import biz.isphere.core.spooledfiles.SpooledFile;
+
 import com.ibm.etools.iseries.rse.ui.actions.popupmenu.ISeriesAbstractQSYSPopupMenuAction;
 
-import biz.isphere.core.spooledfiles.SpooledFile;
 import de.tools400.rpgunit.core.extensions.testcase.IRPGUnitTestCaseItem;
 import de.tools400.rpgunit.core.extensions.view.IRPGUnitSpooledFile;
 import de.tools400.rpgunit.isphere.Messages;
@@ -26,30 +28,36 @@ public abstract class AbstractSpooledFileAction extends ISeriesAbstractQSYSPopup
 
         String message = null;
 
-        Object[] selection = getSelectedRemoteObjects();
-        for (int i = 0; i < selection.length; i++) {
-            if (selection[i] instanceof IRPGUnitTestCaseItem) {
-                IRPGUnitTestCaseItem testCaseItem = (IRPGUnitTestCaseItem)selection[i];
-                IRPGUnitSpooledFile rpgunitSpooledFile = testCaseItem.getSpooledFile();
-                if (rpgunitSpooledFile != null) {
-                    SpooledFile spooledFile = SpooledFileFactory.createSpooledFile(rpgunitSpooledFile);
-                    if (spooledFile != null) {
-                        message = execute(spooledFile);
+        try {
+
+            Object[] selection = getSelectedRemoteObjects();
+            for (int i = 0; i < selection.length; i++) {
+                if (selection[i] instanceof IRPGUnitTestCaseItem) {
+                    IRPGUnitTestCaseItem testCaseItem = (IRPGUnitTestCaseItem)selection[i];
+                    IRPGUnitSpooledFile rpgunitSpooledFile = testCaseItem.getSpooledFile();
+                    if (rpgunitSpooledFile != null) {
+                        SpooledFile spooledFile = SpooledFileFactory.createSpooledFile(rpgunitSpooledFile);
+                        if (spooledFile != null) {
+                            message = execute(spooledFile);
+                        } else {
+                            message = Messages.No_spooled_file_available;
+                        }
                     } else {
                         message = Messages.No_spooled_file_available;
                     }
-                } else {
-                    message = Messages.No_spooled_file_available;
+                }
+                if (message != null) {
+                    MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.Error, message);
+                    break;
                 }
             }
-            if (message != null) {
-                MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.Error, message);
-                break;
-            }
+
+        } catch (CanceledByUserException e) {
+            // nothing to do here
         }
 
     }
 
-    public abstract String execute(SpooledFile spooledFile);
+    public abstract String execute(SpooledFile spooledFile) throws CanceledByUserException;
 
 }
--- eclipse/iRPGUnit iSphere Integration Plugin/src/de/tools400/rpgunit/isphere/action/SpooledFileOpenAsPDFAction.java
+++ eclipse/iRPGUnit iSphere Integration Plugin/src/de/tools400/rpgunit/isphere/action/SpooledFileOpenAsPDFAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013-2016 iRPGUnit Project Team
+ * Copyright (c) 2013-2020 iRPGUnit Project Team
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
 
 package de.tools400.rpgunit.isphere.action;
 
+import biz.isphere.core.internal.exception.CanceledByUserException;
 import biz.isphere.core.preferencepages.IPreferences;
 import biz.isphere.core.spooledfiles.SpooledFile;
 
@@ -16,7 +17,7 @@ public class SpooledFileOpenAsPDFAction extends AbstractSpooledFileAction {
     public static final String ID = "de.tools400.rpgunit.isphere.action.SpooledFileOpenAsPDFAction"; //$NON-NLS-1$
 
     @Override
-    public String execute(SpooledFile spooledFile) {
+    public String execute(SpooledFile spooledFile) throws CanceledByUserException {
         return spooledFile.open(IPreferences.OUTPUT_FORMAT_PDF);
     }
 }
