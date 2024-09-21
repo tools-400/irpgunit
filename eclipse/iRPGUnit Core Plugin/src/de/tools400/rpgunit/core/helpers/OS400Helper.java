c07ea3ae42693f5f5e3e50c5b0523ee3f1e51ef7
Thomas Raddatz <thomas.raddatz@obi.de>
2024-09-21 16:35:35 +0200

    Remove obsolete classes.

--- eclipse/iRPGUnit Core Plugin/src/de/tools400/rpgunit/core/helpers/OS400Helper.java
+++ eclipse/iRPGUnit Core Plugin/src/de/tools400/rpgunit/core/helpers/OS400Helper.java
@@ -11,9 +11,6 @@ package de.tools400.rpgunit.core.helpers;
 import com.ibm.as400.access.AS400;
 import com.ibm.as400.access.CommandCall;
 
-import de.tools400.rpgunit.core.Messages;
-import de.tools400.rpgunit.core.host.GetPTFStatus;
-
 public final class OS400Helper {
 
     public static boolean executeCommand(AS400 system, String command) throws Exception {
@@ -29,21 +26,6 @@ public final class OS400Helper {
 
     public static String getRelease(AS400 system) throws Exception {
         return new OS400Release(system).getRelease();
-    }
-
-    public static String getPTFStatus(AS400 system, String ptf) throws Exception {
-
-        GetPTFStatus program = new GetPTFStatus();
-        if (!program.run(system, ptf)) {
-            String[] errorMessages = program.getErrorMessages();
-            if (errorMessages.length > 0) {
-                throw new Exception(Messages.bind(Messages.Could_not_execute_program_A_Reason_B, program.getProgramName(), errorMessages[0]));
-            } else {
-                throw new Exception(Messages.bind(Messages.Could_not_execute_program_A_Reason_B, program.getProgramName(), "unknown"));
-            }
-        }
-
-        return program.getPTFStatus();
     }
 
     public static String getReleaseShort(AS400 system) throws Exception {
