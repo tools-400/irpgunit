dba976574a5075d9a9a0a0a06c8f6966802db933
Thomas Raddatz <thomas.raddatz@obi.de>
2026-02-06 16:04:06 +0100

    Fix publishing html help pages.

---
+++ docs/help/assets/menu.js
@@ -0,0 +1,115 @@
+// ---------------------------------------------------------- //
+//  http://www.dynamicdrive.com/dynamicindex1/navigate1.htm   //
+// ---------------------------------------------------------- //
+document.write('\<ul id="treemenu" class="treeview">\
+  <li><a href="../html/introduction/introduction.html" target="content" style="text-decoration:none">Introduction</a></li>\
+  <li><a href="../html/installation/installation.html" target="content" style="text-decoration:none">Installation</a></li>\
+  <li>Upgrade Instructions\
+  <ul>\
+    <li><a href="../html/upgrade/upgrade_032.html" target="content" style="text-decoration:none">Upgrading from v0.3.2 to v2.0.0</a></li>\
+    <li><a href="../html/upgrade/upgrade_110.html" target="content" style="text-decoration:none">Upgrading from v1.10 to v2.0.0</a></li>\
+    <li><a href="../html/upgrade/upgrade_242.html" target="content" style="text-decoration:none">Upgrading from v2.4.2 to v3.0.0</a></li>\
+    <li><a href="../html/upgrade/upgrade_320.html" target="content" style="text-decoration:none">Upgrading from v3.2.0 to v4.0.0</a></li>\
+    <li><a href="../html/upgrade/upgrade_404.html" target="content" style="text-decoration:none">Upgrading from v4.0.4 to v4.1.0</a></li>\
+    <li><a href="../html/upgrade/upgrade_421.html" target="content" style="text-decoration:none">Upgrading from v4.2.1 to v5.0.0</a></li>\
+    <li><a href="../html/upgrade/upgrade_510.html" target="content" style="text-decoration:none">Upgrading from v5.1.0 to v6.0.0</a></li>\
+  </ul>\
+  </li>\
+  <li>Quickstart\
+  <ul>\
+    <li><a href="../html/quickstart/preferences.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    <li><a href="../html/quickstart/usage.html" target="content" style="text-decoration:none">Usage</a>\
+    <ul>\
+      <li><a href="../html/quickstart/cobol/usage_cobol.html" target="content" style="text-decoration:none">Cobol</a></li>\
+      <li><a href="../html/quickstart/stmf/usage_stmf.html" target="content" style="text-decoration:none">Stream Files</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/quickstart/debug.html" target="content" style="text-decoration:none">Debugging Test Cases</a></li>\
+    <li><a href="../html/quickstart/codecoverage.html" target="content" style="text-decoration:none">Code Coverage</a></li>\
+    <li><a href="../html/quickstart/notices.html" target="content" style="text-decoration:none">Notices</a></li>\
+  </ul>\
+  </li>\
+  <li>API Reference\
+  <ul>\
+    <li><a href="../html/reference/setupTeardownOverview.html" target="content" style="text-decoration:none">Set up and tear down</a>\
+    <ul>\
+      <li><a href="../html/reference/setupTestSuite.html" target="content" style="text-decoration:none">Set up test suite</a></li>\
+      <li><a href="../html/reference/setupTestCase.html" target="content" style="text-decoration:none">Set up test case</a></li>\
+      <li><a href="../html/reference/teardownTestCase.html" target="content" style="text-decoration:none">Tear down test case</a></li>\
+      <li><a href="../html/reference/teardownTestSuite.html" target="content" style="text-decoration:none">Tear down test suite</a></li>\
+    </ul>\
+    </li>\
+    <li>Assertions\
+    <ul>\
+      <li><a href="../html/reference/assertEqual.html" target="content" style="text-decoration:none">assertEqual</a></li>\
+      <li><a href="../html/reference/assertThat.html" target="content" style="text-decoration:none">assertThat</a></li>\
+      <li><a href="../html/reference/aEqual.html" target="content" style="text-decoration:none">aEqual</a></li>\
+      <li><a href="../html/reference/iEqual.html" target="content" style="text-decoration:none">iEqual</a></li>\
+      <li><a href="../html/reference/nEqual.html" target="content" style="text-decoration:none">nEqual</a></li>\
+      <li><a href="../html/reference/assert.html" target="content" style="text-decoration:none">assert</a></li>\
+      <li><a href="../html/reference/fail.html" target="content" style="text-decoration:none">fail</a></li>\
+      <li><a href="../html/reference/getMonitoredMessage.html" target="content" style="text-decoration:none">getMonitoredMessage</a></li>\
+      <li><a href="../html/reference/assertJobLogContains.html" target="content" style="text-decoration:none">assertJobLogContains</a></li>\
+      <li><a href="../html/reference/assertMessageQueueContains.html" target="content" style="text-decoration:none">assertMessageQueueContains</a></li>\
+    </ul>\
+    </li>\
+    <li>Utilities\
+    <ul>\
+      <li><a href="../html/reference/getFullTimestamp.html" target="content" style="text-decoration:none">getFullTimeStamp</a></li>\
+    </ul>\
+    </li>\
+    <li>CL Commands\
+    <ul>\
+      <li><a href="../html/reference/clrpfm.html" target="content" style="text-decoration:none">CLRPFM</a></li>\
+      <li><a href="../html/reference/rclactgrp.html" target="content" style="text-decoration:none">RCLACTGRP</a></li>\
+      <li><a href="../html/reference/runCmd.html" target="content" style="text-decoration:none">runCmd</a></li>\
+      <li><a href="../html/reference/waitSeconds.html" target="content" style="text-decoration:none">waitSeconds</a></li>\
+      <li><a href="../html/reference/displayStatusMessage.html" target="content" style="text-decoration:none">displayStatusMessage</a></li>\
+      <li><a href="../html/reference/restoreStatusMessage.html" target="content" style="text-decoration:none">restoreStatusMessage</a></li>\
+      <li><a href="../html/reference/clearStatusMessage.html" target="content" style="text-decoration:none">clearStatusMessage</a></li>\
+      <li><a href="../html/reference/getMemberType.html" target="content" style="text-decoration:none">getMemberType</a></li>\
+      <li><a href="../html/reference/getStmfType.html" target="content" style="text-decoration:none">getStmfType</a></li>\
+      <li><a href="../html/reference/setLowMessageKey.html" target="content" style="text-decoration:none">setLowMessageKey</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/reference/selftest.html" target="content" style="text-decoration:none">Self-Test</a>\
+    <ul>\
+      <li><a href="../html/reference/getAssertFailEvt.html" target="content" style="text-decoration:none">getAssertFailEvt</a></li>\
+      <li><a href="../html/reference/getAssertFailEvtLong.html" target="content" style="text-decoration:none">getAssertFailEvtLong</a></li>\
+      <li><a href="../html/reference/getAssertCalled.html" target="content" style="text-decoration:none">getAssertCalled</a></li>\
+      <li><a href="../html/reference/clearAssertCounter.html" target="content" style="text-decoration:none">clearAssertCounter</a></li>\
+      <li><a href="../html/reference/clrAssertFailEvt.html" target="content" style="text-decoration:none">clrAssertFailEvt</a></li>\
+    </ul>\
+    </li>\
+  </ul>\
+  </li>\
+  <li><a href="../html/integration/integration.html" target="content" style="text-decoration:none">Integration</a>\
+  <ul>\
+    <li><a href="../html/integration/cmone/usage_cmone.html" target="content" style="text-decoration:none">CMOne</a>\
+    <ul>\
+      <li><a href="../html/integration/cmone/preferences/preferences_cmone.html" target="content" style="text-decoration:none">Preferences</a></li>\
+    </ul>\
+    </li>\
+    <li><a href="../html/integration/jenkins/usage_jenkins.html" target="content" style="text-decoration:none">Jenkins</a></li>\
+    <li><a href="../html/integration/vscode/usage_vscode.html" target="content" style="text-decoration:none">VS Code</a></li>\
+  </ul>\
+  </li>\
+  <li>Troubleshooting\
+  <ul>\
+    <li><a href="../html/troubleshooting/duplicate_irpgunit_objects.html" target="content" style="text-decoration:none">Error: Duplicate iRPGUnit objects</a></li>\
+    <li><a href="../html/troubleshooting/updlib_error.html" target="content" style="text-decoration:none">Error: Could not update library</a></li>\
+  </ul>\
+  </li>\
+  <li><a href="../html/contributors/contributors.html" target="content" style="text-decoration:none">Contributors</a></li>\
+  <li>Appendix\
+  <ul>\
+    <li><a href="../html/appendix/example_1.html" target="content" style="text-decoration:none">iRPGUnit RPG Example</a></li>\
+    <li><a href="../html/appendix/example_2.html" target="content" style="text-decoration:none">iRPGUnit Cobol Example</a></li>\
+    <li><a href="../html/appendix/output_jenkins_1.html" target="content" style="text-decoration:none">XML Output Jenkins Type 1</a></li>\
+    <li><a href="../html/appendix/output_jenkins_2.html" target="content" style="text-decoration:none">XML Output Jenkins Type 2</a></li>\
+    <li><a href="../html/appendix/output_vscode_1.html" target="content" style="text-decoration:none">XML Output VS Code Type 2</a></li>\
+    <li><a href="../html/appendix/upload_save_file.html" target="content" style="text-decoration:none">Upload Save File .cmd</a></li>\
+  </ul>\
+  </li>\
+</ul>\
+');
